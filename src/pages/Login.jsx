18c9cb01b0ce056b81862511c63c6ebf9a19bae7
Amine Moumen <aamine.moumen@outlook.fr>
2024-11-14 15:35:33 +0100

    feat : Ajout du lien vers la page Album sur la page Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <>
@@ -23,9 +25,11 @@ function Login() {
                 className="w-full h-12 text-gray-900 placeholder:text-gray-400 text-lg font-normal leading-7 rounded-full border-gray-300 border shadow-sm focus:outline-none px-4 mb-1"
                 placeholder="Mot de passe"
               />
-              <button className="w-full mt-6 h-12 text-white text-center text-base font-semibold leading-6 rounded-full hover:bg-secondary transition-all duration-300 bg-accent shadow-sm mb-11">
-                Se connecter
-              </button>
+              <Link to="/album">
+                <button className="w-full mt-6 h-12 text-white text-center text-base font-semibold leading-6 rounded-full hover:bg-secondary transition-all duration-300 bg-accent shadow-sm mb-11">
+                  Se connecter
+                </button>
+              </Link>
             </form>
           </div>
         </div>
